5abffb279fa657f3d712cbf5b544e31d426cce7d
Anton Mishkurov <Anton_Mishkurov@epam.com>
2016-09-30 13:42:14 +0300

    Just to save work... Need make report works fine

--- src/main/java/com/mishkurov/FitnessService.java
+++ src/main/java/com/mishkurov/FitnessService.java
@@ -1,14 +1,14 @@
 package com.mishkurov;
 
 import java.time.LocalDate;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class FitnessService {
     public static final double PACES_PER_DAY = 2000;
     public static final double DRINK_PER_DAY = 1500.;
     public static final double MOVE_SECONDS_PER_DAY = 7200;
     public static final double CALORIES_PER_DAY = 2300.;
+    public static final double EPSILON = 1E-6;
     private Map<LocalDate, Map<Activity, Double>> allData;
 
     public FitnessService() {
@@ -36,36 +36,16 @@ public class FitnessService {
         return storedAmount == null ? 0 : storedAmount;
     }
 
-    @Deprecated
-    public void drink(LocalDate date, Double amount) {
-        addAmount(date, Activity.DRINK, amount);
-    }
-
     public double getDrinkAmount(LocalDate date) {
         return getAmount(date, Activity.DRINK);
-    }
-
-    @Deprecated
-    public void eat(LocalDate date, Double calories) {
-        addAmount(date, Activity.EAT, calories);
     }
 
     public Double getEatAmount(LocalDate date) {
         return getAmount(date, Activity.EAT);
     }
 
-    @Deprecated
-    public void move(LocalDate date, double seconds) {
-        addAmount(date, Activity.MOVE,  seconds);
-    }
-
     public double getMoveSecondsAmount(LocalDate date) {
         return getAmount(date, Activity.MOVE);
-    }
-
-    @Deprecated
-    public void pace(LocalDate date, double paces) {
-        addAmount(date, Activity.PACE, paces);
     }
 
     public double getPacesAmount(LocalDate date) {
@@ -92,15 +72,26 @@ public class FitnessService {
         return CALORIES_PER_DAY - consumedCalories;
     }
 
-    public Report getReport(LocalDate reportDate) {
-        StringBuilder report = new StringBuilder("Report\n");
-//        for () {
-//        }
-//
-        return null;
+    public double getPercentForActivity(LocalDate date, Activity activity) {
+        switch (activity) {
+            case EAT:
+                return (allData.get(date).get(activity) / CALORIES_PER_DAY) * 100.;
+            case DRINK:
+                return (allData.get(date).get(activity) / DRINK_PER_DAY) * 100.;
+            case MOVE:
+                return (allData.get(date).get(activity) / MOVE_SECONDS_PER_DAY) * 100.;
+            case PACE:
+                return (allData.get(date).get(activity) / PACES_PER_DAY) * 100.;
+            default:
+                return 0;
+        }
     }
 
-    public static class Report {
+    public Report getReport(LocalDate startDate, LocalDate endDate) {
+        return new Report(startDate, endDate);
+    }
+
+    public class Report {
         private double eatPercent;
         private double drinkPercent;
         private double movePercent;
@@ -109,12 +100,19 @@ public class FitnessService {
         private double drinkMedian;
         private double moveMedian;
         private double paceMedian;
+        private Map<Activity, Double> activityPercentMap;
+        private Map<Activity, Double> activityMedianMap;
         private LocalDate startDate;
         private LocalDate endDate;
 
         public Report(LocalDate startDate, LocalDate endDate) {
+            if (startDate.isAfter(endDate)) {
+                throw new IllegalArgumentException("Start date must be before end date!");
+            }
             this.startDate = startDate;
             this.endDate = endDate;
+            activityPercentMap = new HashMap<>();
+            activityMedianMap = new HashMap<>();
             this.eatPercent = 0;
             this.drinkPercent = 0;
             this.movePercent = 0;
@@ -123,14 +121,38 @@ public class FitnessService {
             this.drinkMedian = 0;
             this.moveMedian = 0;
             this.paceMedian = 0;
+            calculate();
         }
 
-        public LocalDate getStartDate() {
-            return startDate;
-        }
+        private void calculate() {
+            LocalDate currDate = startDate;
+            Map<Activity, ArrayList<Double>> percentsPerDay = new HashMap<>();
+            for (Activity a : Activity.values()) {
+                percentsPerDay.put(a, new ArrayList<>());
+            }
+            while (!currDate.isAfter(endDate)) {
+                Map<Activity, Double> dataPerDay = allData.get(currDate);
+                for (Activity a : Activity.values()) {
+                    Double valueForActivity = dataPerDay.get(a);
+                    valueForActivity = valueForActivity == null ? 0 : valueForActivity;
+                    percentsPerDay.get(a).add(valueForActivity);
+                }
+                currDate = currDate.plusDays(1);
+            }
+            for (Activity a : Activity.values()) {
+                ArrayList<Double> activityValues = percentsPerDay.get(a);
+                Collections.sort(activityValues);
+                Double median = 0.;
+                int numberOfValues = activityValues.size();
+                if (numberOfValues % 2 == 0) {
+                    median = (activityValues.get(numberOfValues / 2) + activityValues.get(numberOfValues / 2 + 1)) / 2;
+                } else {
+                    median = activityValues.get(numberOfValues / 2);
+                }
+                activityMedianMap.put(a, median);
 
-        public LocalDate getEndDate() {
-            return endDate;
+//                activityPercentMap.put(a, activityValues)
+            }
         }
 
         public void setActivityPercent(Activity activity, double percent) {
@@ -200,19 +222,30 @@ public class FitnessService {
         @Override
         public boolean equals(Object otherReport) {
             if (otherReport instanceof Report) {
-                //todo replace with double comparison
-                return (((Report) otherReport).eatPercent == this.eatPercent &&
-                        ((Report) otherReport).drinkPercent == this.drinkPercent &&
-                        ((Report) otherReport).movePercent == this.movePercent &&
-                        ((Report) otherReport).pacePercent == this.pacePercent &&
-                        ((Report) otherReport).eatMedian == this.eatMedian &&
-                        ((Report) otherReport).drinkMedian == this.drinkMedian &&
-                        ((Report) otherReport).moveMedian == this.moveMedian &&
-                        ((Report) otherReport).paceMedian == this.paceMedian);
+                return (Math.abs(Double.compare(((Report) otherReport).eatPercent, this.eatPercent)) < EPSILON &&
+                        Math.abs(Double.compare(((Report) otherReport).drinkPercent, this.drinkPercent)) < EPSILON &&
+                        Math.abs(Double.compare(((Report) otherReport).movePercent, this.movePercent)) < EPSILON &&
+                        Math.abs(Double.compare(((Report) otherReport).pacePercent, this.pacePercent)) < EPSILON &&
+                        Math.abs(Double.compare(((Report) otherReport).eatMedian, this.eatMedian)) < EPSILON &&
+                        Math.abs(Double.compare(((Report) otherReport).drinkMedian, this.drinkMedian)) < EPSILON &&
+                        Math.abs(Double.compare(((Report) otherReport).moveMedian, this.moveMedian)) < EPSILON &&
+                        Math.abs(Double.compare(((Report) otherReport).paceMedian, this.paceMedian)) < EPSILON);
             } else {
                 return false;
             }
         }
+
+        @Override
+        public String toString() {
+            StringBuilder report = new StringBuilder();
+            report.append("Start date: " + this.startDate + " End date: " + this.endDate + "\n");
+            for (Activity a : Activity.values()) {
+                report.append("Percent for (" + a + ")=" + activityPercentMap.get(a) + ", ");
+                report.append("Median for (" + a + ")=" + activityMedianMap.get(a) + ", ");
+            }
+            return report.toString();
+        }
     }
+
 }
 
--- src/test/java/com/mishkurov/FitnessServiceTest.java
+++ src/test/java/com/mishkurov/FitnessServiceTest.java
@@ -14,7 +14,6 @@ import static org.junit.Assert.*;
 
 public class FitnessServiceTest {
 
-    public static final double EPSILON = 1E-6;
     private FitnessService service;
 
     @Before
@@ -44,49 +43,49 @@ public class FitnessServiceTest {
     @Test
     public void drink() {
         Double amount = 100.;
-        service.drink(LocalDate.parse("2016-09-27"), amount);
-        service.drink(LocalDate.parse("2016-09-28"), amount);
-        service.drink(LocalDate.parse("2016-09-28"), amount);
-        service.drink(LocalDate.parse("2016-09-29"), amount);
-        assertThat(service.getAmount(LocalDate.parse("2016-09-28"),Activity.DRINK), closeTo(amount * 2., EPSILON));
-        assertThat(service.getAmount(LocalDate.parse("2016-09-27"),Activity.DRINK), closeTo(amount * 1., EPSILON));
-        assertThat(service.getAmount(LocalDate.parse("2016-09-26"),Activity.DRINK), closeTo(amount * 0., EPSILON));
+        service.addAmount(LocalDate.parse("2016-09-27"), Activity.DRINK, amount);
+        service.addAmount(LocalDate.parse("2016-09-28"), Activity.DRINK, amount);
+        service.addAmount(LocalDate.parse("2016-09-28"), Activity.DRINK, amount);
+        service.addAmount(LocalDate.parse("2016-09-29"), Activity.DRINK, amount);
+        assertThat(service.getAmount(LocalDate.parse("2016-09-28"), Activity.DRINK), closeTo(amount * 2., EPSILON));
+        assertThat(service.getAmount(LocalDate.parse("2016-09-27"), Activity.DRINK), closeTo(amount * 1., EPSILON));
+        assertThat(service.getAmount(LocalDate.parse("2016-09-26"), Activity.DRINK), closeTo(amount * 0., EPSILON));
     }
 
     @Test
     public void eat() {
         Double calories = 15.;
-        service.eat(LocalDate.parse("2016-09-27"), calories);
-        service.eat(LocalDate.parse("2016-09-25"), calories);
-        service.eat(LocalDate.parse("2016-09-25"), calories);
-        service.eat(LocalDate.parse("2016-09-27"), calories);
-        assertThat(service.getAmount(LocalDate.parse("2016-09-25"),Activity.EAT), closeTo(calories * 2, EPSILON));
-        assertThat(service.getAmount(LocalDate.parse("2016-09-27"),Activity.EAT), closeTo(calories * 2, EPSILON));
+        service.addAmount(LocalDate.parse("2016-09-27"), Activity.EAT, calories);
+        service.addAmount(LocalDate.parse("2016-09-25"), Activity.EAT, calories);
+        service.addAmount(LocalDate.parse("2016-09-25"), Activity.EAT, calories);
+        service.addAmount(LocalDate.parse("2016-09-27"), Activity.EAT, calories);
+        assertThat(service.getAmount(LocalDate.parse("2016-09-25"), Activity.EAT), closeTo(calories * 2, EPSILON));
+        assertThat(service.getAmount(LocalDate.parse("2016-09-27"), Activity.EAT), closeTo(calories * 2, EPSILON));
     }
 
     @Test
     public void move() {
         double seconds = 3600;
-        service.move(LocalDate.parse("2016-09-27"), seconds);
-        service.move(LocalDate.parse("2016-09-27"), seconds);
-        assertThat(service.getAmount(LocalDate.parse("2016-09-27"),Activity.MOVE), is(seconds * 2.));
-        assertThat(service.getAmount(LocalDate.parse("2016-09-25"),Activity.MOVE), is(seconds * 0.));
+        service.addAmount(LocalDate.parse("2016-09-27"), Activity.MOVE, seconds);
+        service.addAmount(LocalDate.parse("2016-09-27"), Activity.MOVE, seconds);
+        assertThat(service.getAmount(LocalDate.parse("2016-09-27"), Activity.MOVE), is(seconds * 2.));
+        assertThat(service.getAmount(LocalDate.parse("2016-09-25"), Activity.MOVE), is(seconds * 0.));
     }
 
     @Test
     public void pace() {
         double paces = 1234;
-        service.pace(LocalDate.parse("2016-09-27"), paces);
-        service.pace(LocalDate.parse("2016-09-27"), paces);
-        service.pace(LocalDate.parse("2016-09-27"), paces);
-        assertThat(service.getAmount(LocalDate.parse("2016-09-27"),Activity.PACE), is(paces * 3));
-        assertThat(service.getAmount(LocalDate.parse("2016-09-22"),Activity.PACE), is(paces * 0));
+        service.addAmount(LocalDate.parse("2016-09-27"), Activity.PACE, paces);
+        service.addAmount(LocalDate.parse("2016-09-27"), Activity.PACE, paces);
+        service.addAmount(LocalDate.parse("2016-09-27"), Activity.PACE, paces);
+        assertThat(service.getAmount(LocalDate.parse("2016-09-27"), Activity.PACE), is(paces * 3));
+        assertThat(service.getAmount(LocalDate.parse("2016-09-22"), Activity.PACE), is(paces * 0));
     }
 
     @Test
     public void pacesLeft() {
         double paces = 1234;
-        service.pace(LocalDate.parse("2016-09-27"), paces);
+        service.addAmount(LocalDate.parse("2016-09-27"), Activity.PACE, paces);
         assertThat(service.getPacesLeft(LocalDate.parse("2016-09-27")), is(PACES_PER_DAY - paces));
         assertThat(service.getPacesLeft(LocalDate.parse("2016-09-26")), is(PACES_PER_DAY));
     }
@@ -94,8 +93,8 @@ public class FitnessServiceTest {
     @Test
     public void drinkLeft() {
         double drink = 220.32;
-        service.drink(LocalDate.parse("2016-09-28"), drink);
-        service.drink(LocalDate.parse("2016-09-28"), drink);
+        service.addAmount(LocalDate.parse("2016-09-28"), Activity.DRINK, drink);
+        service.addAmount(LocalDate.parse("2016-09-28"), Activity.DRINK, drink);
         assertThat(service.getDrinkLeft(LocalDate.parse("2016-09-28")), closeTo(DRINK_PER_DAY - drink * 2, EPSILON));
         assertThat(service.getDrinkLeft(LocalDate.parse("2016-09-27")), closeTo(DRINK_PER_DAY - drink * 0, EPSILON));
     }
@@ -103,37 +102,54 @@ public class FitnessServiceTest {
     @Test
     public void moveLeft() {
         double seconds = 3600;
-        service.move(LocalDate.parse("2016-09-27"), seconds);
+        service.addAmount(LocalDate.parse("2016-09-27"), Activity.MOVE, seconds);
         assertThat(service.getMoveSecondsLeft(LocalDate.parse("2016-09-27")), is(MOVE_SECONDS_PER_DAY - seconds));
     }
 
     @Test
     public void eatLeft() {
         double calories = 254.3;
-        service.eat(LocalDate.parse("2016-09-27"), calories);
-        service.eat(LocalDate.parse("2016-09-27"), calories);
-        service.eat(LocalDate.parse("2016-09-26"), calories);
+        service.addAmount(LocalDate.parse("2016-09-27"), Activity.EAT, calories);
+        service.addAmount(LocalDate.parse("2016-09-27"), Activity.EAT, calories);
+        service.addAmount(LocalDate.parse("2016-09-26"), Activity.EAT, calories);
         assertThat(service.getCaloriesLeft(LocalDate.parse("2016-09-27")), closeTo(CALORIES_PER_DAY - calories * 2, EPSILON));
         assertThat(service.getCaloriesLeft(LocalDate.parse("2016-09-26")), closeTo(CALORIES_PER_DAY - calories * 1, EPSILON));
         assertThat(service.getCaloriesLeft(LocalDate.parse("2016-09-25")), closeTo(CALORIES_PER_DAY - calories * 0, EPSILON));
     }
 
     @Test
-    @Ignore
     public void reportPerDay() {
         LocalDate reportDate = LocalDate.parse("2016-09-27");
-        FitnessService.Report serviceReport = service.getReport(reportDate);
-        FitnessService.Report idealReport = new FitnessService.Report(reportDate, reportDate) {{
-            setActivityPercent(Activity.EAT, 10.);
-            setActivityPercent(Activity.DRINK, 10.);
-            setActivityPercent(Activity.PACE, 10.);
-            setActivityPercent(Activity.MOVE, 10.);
-            setMedian(Activity.EAT, 10.);
-            setMedian(Activity.DRINK, 10.);
-            setMedian(Activity.PACE, 10.);
-            setMedian(Activity.MOVE, 10.);
+        for (int i = 0; i++ < 10; ) {
+            service.addAmount(reportDate, Activity.EAT, i * 1.);  //45
+            service.addAmount(reportDate, Activity.DRINK, i * 10.); //450
+            service.addAmount(reportDate, Activity.PACE, i * 100.); //4500
+            service.addAmount(reportDate, Activity.MOVE, 100.); //1000
+        }
+        FitnessService.Report serviceReport = service.getReport(reportDate, reportDate);
+        FitnessService.Report idealReport =  service.new Report(reportDate, reportDate) {{
+//            setActivityPercent(Activity.EAT, 45 / CALORIES_PER_DAY * 100);
+//            setActivityPercent(Activity.DRINK, 450 / DRINK_PER_DAY * 100);
+//            setActivityPercent(Activity.PACE, 4500 / PACES_PER_DAY * 100);
+//            setActivityPercent(Activity.MOVE, 1000 / MOVE_SECONDS_PER_DAY * 100);
+            setMedian(Activity.EAT, 4.5);
+            setMedian(Activity.DRINK, 45);
+            setMedian(Activity.PACE, 450.);
+            setMedian(Activity.MOVE, 100.);
         }};
-        assertThat(idealReport, is(idealReport));
+        System.out.println("service Report");
+        System.out.println(serviceReport);
+        System.out.println("ideal Report");
+        System.out.println(idealReport);
+        assertThat(serviceReport, is(idealReport));
     }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testStartEndDatesValues() {
+        LocalDate startDate = LocalDate.parse("2016-09-27");
+        LocalDate endDate = LocalDate.parse("2016-09-26");
+        FitnessService.Report report = service.getReport(startDate, endDate);
+    }
+
 
 }
