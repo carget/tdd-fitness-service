c5212fb1b0eac2c7708b5826ab9720721c16f40f
carget <carget@users.noreply.github.com>
2016-10-01 13:13:20 +0300

    Improve activity to store default values

--- src/main/java/com/mishkurov/Activity.java
+++ src/main/java/com/mishkurov/Activity.java
@@ -1,8 +1,17 @@
 package com.mishkurov;
 
-/**
- * @author Anton_Mishkurov
- */
 public enum Activity {
-    EAT, DRINK, MOVE, PACE;
+
+    EAT(1000.), DRINK(1000.), MOVE(1000.), PACE(1000.);
+
+    private final double defaultValue;
+
+    Activity(double defaultValue) {
+        this.defaultValue = defaultValue;
+    }
+
+    public double getDefaultValue() {
+        return defaultValue;
+    }
+
 }
